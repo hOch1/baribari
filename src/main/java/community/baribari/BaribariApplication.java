b6322b79ab7dc768a240af6b22240414be000578
h0ch1 <h0ch1@naver.com>
2024-06-09 13:41:37 +0900

    first commit

---
+++ src/main/java/community/baribari/BaribariApplication.java
@@ -0,0 +1,13 @@
+package community.baribari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaribariApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaribariApplication.class, args);
+    }
+
+}
---
+++ src/test/java/community/baribari/BaribariApplicationTests.java
@@ -0,0 +1,13 @@
+package community.baribari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaribariApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
