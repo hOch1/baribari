d81572dc10af213e842d625fbd929d9ecf1e5e79
h0ch1 <h0ch1@naver.com>
2024-06-22 18:22:33 +0900

    메인페이지 레이아웃 변경 및 summernote 적용

--- src/main/java/community/baribari/controller/bari/BariController.java
+++ src/main/java/community/baribari/controller/bari/BariController.java
@@ -1,15 +1,23 @@
 package community.baribari.controller.bari;
 
+import community.baribari.config.PrincipalDetail;
+import community.baribari.entity.Member;
+import community.baribari.service.BariService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/bari")
 @RequiredArgsConstructor
 public class BariController {
+
+    private final BariService bariService;
 
     @GetMapping("/recruit")
     public String recruit(Model model) {
@@ -19,5 +27,12 @@ public class BariController {
     @GetMapping("/review")
     public String review(Model model) {
         return "bari/bari-review";
+    }
+
+    @PostMapping("/write.do")
+    public String write(@RequestParam String title, @RequestParam String content,
+                        @AuthenticationPrincipal PrincipalDetail principalDetail) {
+        bariService.save(title, content, principalDetail);
+        return "redirect:/";
     }
 }
