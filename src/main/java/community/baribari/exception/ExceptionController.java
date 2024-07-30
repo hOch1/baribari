9ef8d301bef888f3e433945af8e98dee20539ca2
h0ch1 <h0ch1@naver.com>
2024-07-30 15:58:09 +0900

    HandlerMethodValidationException 예외처리 추가

--- src/main/java/community/baribari/exception/ExceptionController.java
+++ src/main/java/community/baribari/exception/ExceptionController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @ControllerAdvice
@@ -21,7 +22,20 @@ public class ExceptionController {
     public String methodArgumentNotValidException(MethodArgumentNotValidException e,
                                                   RedirectAttributes redirectAttributes,
                                                   HttpServletRequest request){
-        redirectAttributes.addFlashAttribute("message", e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+        redirectAttributes.addFlashAttribute("message", e.getBindingResult()
+                .getAllErrors()
+                .get(0)
+                .getDefaultMessage());
+        return "redirect:"+request.getHeader("Referer");
+    }
+
+    @ExceptionHandler(HandlerMethodValidationException.class)
+    public String handlerMethodValidationException(HandlerMethodValidationException e,
+                                                   RedirectAttributes redirectAttributes,
+                                                   HttpServletRequest request){
+        redirectAttributes.addFlashAttribute("message", e.getAllErrors()
+                .get(0)
+                .getDefaultMessage());
         return "redirect:"+request.getHeader("Referer");
     }
 }
